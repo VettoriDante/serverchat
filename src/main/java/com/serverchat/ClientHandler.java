71c9ba4d9c2afd801c1941f11d61e3a5632f961c
lai2-24 <lai2-24@itismeucci.com>
2024-11-05 14:25:05 +0100

    fixes write Byte + new method auth

--- src/main/java/com/serverchat/ClientHandler.java
+++ src/main/java/com/serverchat/ClientHandler.java
@@ -20,63 +20,33 @@ public class ClientHandler extends Thread {
     private User user;
     private ArrayList<Chat> userChats;
     private ArrayList<Group> userGroups;
+    private BufferedReader in ;
+    private DataOutputStream out;
     private Socket socket;
     private Datas datas;
 
     public ClientHandler(Socket socket, Datas datas) {
         this.socket = socket;
         this.datas = datas;
+        try {
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         //try to use the socket
         try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+           
 
             //read user info to establish connection
             boolean error = true;
             do{
-                String typeOfUser = in.readLine();//get the old/new user
-                CommandType c = CommandType.valueOf(typeOfUser);//cast like operation c = command used
-                String inputs = null;
-    
-                inputs = in.readLine();//get data
-    
-                switch (c) {
-                    case NEW_USER:
-                        //for new user wait to be sent all user info 
-                            JsonUser newUserJ = new Gson().fromJson(inputs, JsonUser.class);//transform datas recived into a User
-                            if(newUserJ == null){
-                                out.writeBytes(CommandType.ERR_WRONG_DATA + "\n");//send error
-                                error = true;
-                            }
-                            else{
-                                User newUser = new User(newUserJ.getUsername(), newUserJ.getPassword());
-                                datas.newUser(newUser);//add the new user to the general array of datas 
-                                user = newUser; // set this.user
-                                out.writeBytes(CommandType.OK.toString() + "\n"); //send the ok 
-                            }        
-                        break;
-                    case OLD_USER:
-                            JsonUser searchFor = new Gson().fromJson(inputs, JsonUser.class); 
-                            User tmp = datas.getUser(searchFor.getUsername(), searchFor.getPassword());
-                            //check if the user was successfully found
-                            if(tmp == null){
-                                out.writeBytes(CommandType.ERR_NOT_FOUND + "\n");
-                                error = true;
-                            }
-                            else{
-                                out.writeBytes(CommandType.OK.toString() + "\n"); //send the ok 
-                                user = tmp;
-                            }
-                        break;
-                    default:
-                            out.writeBytes(CommandType.ERR_WRONG_DATA + "\n");
-                            error = true;
-                        break;
-                }
+              error =  Authentication();
             }while(error);
 
             //Once you know who the user is, the server get ready to send him
@@ -88,6 +58,10 @@ public class ClientHandler extends Thread {
             e.printStackTrace();
         }
     }
+    // method for send a string , with before the implementation of "\n"
+    private void WriteBytes(String stringtosout) throws IOException{ 
+        out.writeBytes(stringtosout + "\n");
+    }
     
     //return an array the Gson of ChatToSend and require an array of ChatInterface
     private String getJSONToSend(ArrayList<ChatInterface> chats){
@@ -97,5 +71,53 @@ public class ClientHandler extends Thread {
         }
 
         return new Gson().toJson(toJson);
+    }
+
+    // the method do de authentication and return a boolean error 
+    private Boolean Authentication() throws IOException  {
+
+        String typeOfUser = in.readLine();// get the old/new user
+        CommandType c = CommandType.valueOf(typeOfUser);// cast like operation c = command used
+        String inputs = null;
+        Boolean error = false ;
+
+        inputs = in.readLine();// get data
+
+        switch (c) {
+            case NEW_USER:
+                // for new user wait to be sent all user info
+                JsonUser newUserJ = new Gson().fromJson(inputs, JsonUser.class);// transform datas recived into a User
+                if (newUserJ == null) {
+                    WriteBytes(CommandType.ERR_WRONG_DATA.toString());
+                    error = true;
+                } else {
+                    if(datas.isExitingName(newUserJ.getUsername())){
+                        WriteBytes(CommandType.ERR_USER_EXISTS.toString());
+                    }
+                    User newUser = new User(newUserJ.getUsername(), newUserJ.getPassword());
+                    datas.newUser(newUser);// add the new user to the general array of datas
+                    user = newUser; // set this.user
+                   WriteBytes(CommandType.OK.toString()); // send the ok
+                    
+                }
+                break;
+            case OLD_USER:
+                JsonUser searchFor = new Gson().fromJson(inputs, JsonUser.class);
+                User tmp = datas.getUser(searchFor.getUsername(), searchFor.getPassword());
+                // check if the user was successfully found
+                if (tmp == null) {
+                    WriteBytes(CommandType.ERR_NOT_FOUND.toString() );
+                    error = true;
+                } else {
+                   WriteBytes(CommandType.OK.toString());
+                    user = tmp;
+                }
+                break;
+            default:
+                WriteBytes(CommandType.ERR_WRONG_DATA.toString());
+                error = true;
+                break;
+        }
+        return error; // return error
     }
 }
