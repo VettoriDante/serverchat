4bd5c047faf4968545f681548b7b3b58fbd61bcc
Dante Vettori <vettori.dante@itismeucci.com>
2024-11-26 06:46:40 +0100

    updated README.md

--- src/main/java/com/serverchat/protocol/CommandType.java
+++ src/main/java/com/serverchat/protocol/CommandType.java
@@ -1,50 +1,46 @@
 package com.serverchat.protocol;
 
-
 public enum CommandType {
-    // general commands
-    NEW_USER("Register new user and initialize user session"),
-    OLD_USER("Initialize user session"),
-    REQ_CHATS("Request information about user chats (userId)"),
-    DEL_USER("Request to delete user information"),
-    MENU_BACK("Return to the main menu"),
+    // general commmands
+    OK("The previous command was executed successfully"),
+    INIT("Send initial user's chat array"),
+    EXIT("Exit from the app and close the connection"),
+    
+    // user management commmands
+    NEW_USER("Initialize new user's session"),
+    OLD_USER("Initialize old     user's session"),
+    DEL_USER("Request to delete user's account"),
+    UPD_NAME("Change username"),
+    LOGOUT("Logout from the current account"),
+    
+    // chat management commmands
+    NEW_CHAT("Initiate a new private chat"),
+    NEW_GROUP("Create a new group"),
     NAV_CHAT("Navigate to specified chat or group"),
+    REQ_CHATS("Request information about user chats (userId)"),
+
+    // messages management commmands
     SEND_MSG("Send a new message"),
     RM_MSG("Remove a message"),
     UPD_MSG("Update the content of a message"),
-    UPD_NAME("Change username"),
-    VIEW_ALL_USERS("View all users in the system"),
-    VIEW_ALL_GROUPS("View all groups in the system"),
-    LOGOUT("Logout from the current account"),
-    EXIT("Exit from the app and close the connection"),
-    OK("The previous command was executed successfully"),
-    INIT("Send initial user's chat array"),
 
-    // chat management
-    NEW_CHAT("Initiate a new private chat"),
-    LEAVE_GROUP("Leave a group chat"),
-    DEL_GROUP("Delete a group object"),
-    UPD_GROUP_NAME("Change the name of a group"),
-    NEW_GROUP("Create a new group"),
-    VIEW_GROUP_USERS("View users in a specific group"),
-
-    //Errors management
+    // error management commmands
     ERR_GEN("General server error"),
     ERR_NOT_FOUND("User not found"),
     ERR_CHAT_EXISTS("Private chat already exists"),
     ERR_USER_EXISTS("Username already in use"),
-    ERR_WRONG_DATA("The data entered is incorrect"),
-    ERR_DISCONNECT("Client has disconnected");
+    ERR_DISCONNECT("Client has disconnected"),
+    ERR_WRONG_DATA("The data entered is incorrect");
 
-
+    // attributes
     private final String description;
 
-
+    // constructors
     CommandType(String description) {
         this.description = description;
     }
 
-
+    // getters
     public String getDescription() {
         return description;
     }
